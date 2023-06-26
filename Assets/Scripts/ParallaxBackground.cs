49073199df9118a57ed4933e7c97a94d85619842
LucianRain <aidanbacon@gmail.com>
2023-06-25 19:56:35 -0700

    wip backgrounds

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -2,11 +2,10 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-
     public float parallaxiness;
 
     Vector2 size;
-    Vector3 startPos;
+    float xOffset;
     Transform cam;
     Vector3 oldCamPos;
 
@@ -14,39 +13,18 @@ public class Parallax : MonoBehaviour
     {
         cam = Camera.main.transform;
         oldCamPos = cam.position;
+        xOffset = cam.position.x - transform.position.x;
+        size = new Vector2(GetComponent<SpriteRenderer>().bounds.size.x, GetComponent<SpriteRenderer>().bounds.size.y);
     }
 
     private void Update()
     {
         var camDelta = cam.position - oldCamPos;
         oldCamPos = cam.position;
+        transform.position += camDelta * parallaxiness;
 
-        transform.position += camDelta * parallaxiness; 
+        var xDist = cam.position.x - (transform.position.x + xOffset);
+        if (xDist > size.x) transform.position += Vector3.right * size.x;
+        if (xDist < -size.x) transform.position += Vector3.left * size.x;
     }
-
-    /*public float parallaxiness;
-
-    Vector2 size;
-    Vector3 startPos;
-    GameObject cam;
-    Vector3 oldCamPos;
-
-    void Start() {
-        cam = transform.parent.gameObject;
-        startPos = transform.position;
-        size = new Vector2(GetComponent<SpriteRenderer>().bounds.size.x, GetComponent<SpriteRenderer>().bounds.size.y);
-    }
-
-    void Update() {
-
-        Vector2 delta = new Vector2(cam.transform.position.x * parallaxiness, cam.transform.position.y * parallaxiness);
-        transform.position = new Vector3(startPos.x + delta.x, startPos.y + delta.y, 0);
-
-        float totalDelta = cam.transform.position.x * (1 - parallaxiness);
-        if (totalDelta > startPos.x + size.x) {
-            startPos += new Vector3(size.x, 0, 0);
-        } else if (totalDelta < startPos.x - size.x) {
-            startPos -= new Vector3(size.x, 0, 0);
-        }
-    }*/
 }
